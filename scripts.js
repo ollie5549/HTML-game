3be0f650132d235efc0ed7edd9d2c4f7a99b5425
ollie5549 <78228978+ollie5549@users.noreply.github.com>
2025-06-11 12:55:46 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,10 @@
 
+function playTone() {
+  const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+  const oscillator = audioContext.createOscillator();
+  oscillator.type = "sine";
+  oscillator.frequency.value = 440;
+  oscillator.connect(audioContext.destination);
+  oscillator.start();
+  oscillator.stop(audioContext.currentTime + 1);
+}
