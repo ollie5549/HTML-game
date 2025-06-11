f1003387251ff1a55dae70e77e327586e69e6d73
ollie5549 <78228978+ollie5549@users.noreply.github.com>
2025-06-11 12:35:10 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,14 +1,1 @@
-let audioContext;
 
-try {
-  audioContext =
-    new (window.AudioContext || window.webkitAudioContext)();
-} catch (error) {
-  window.alert(
-    `Sorry, but your browser doesn't support the Web Audio API!`
-  );
-}
-
-if (audioContext !== undefined) {
-  /* Our code goes here */
-}
